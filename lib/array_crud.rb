e2e466bb4e5707dc00603f8b0f53b39feabed6c4
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 00:17:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,7 @@ def add_element_to_end_of_array(array, element)
 def add_element_to_start_of_array(array, element)
   numbers = [1,2,3,4,5]
   numbers.unshift("wow")
+end
   
 
 def remove_element_from_end_of_array(array)
@@ -21,7 +22,6 @@ def remove_element_from_end_of_array(array)
   count_trash = count.pop
 end
   
-end
 
 def remove_element_from_start_of_array(array)
   
