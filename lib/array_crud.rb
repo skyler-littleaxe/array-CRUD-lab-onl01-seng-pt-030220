9b7719b7ffce2feb0242e3d35f2537ff1ce3a7b4
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 00:04:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
   end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  numbers = [1,2,3,4]
+  numbers.push(5)
+  end
 
 def add_element_to_start_of_array(array, element)
   
