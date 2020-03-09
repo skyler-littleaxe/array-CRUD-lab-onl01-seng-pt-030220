6d5c635723a12af9b00f4247cf89b9b74c708cfe
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 00:27:14 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
 def create_an_empty_array
-  
-end
+  empty_array = []
+  end
 
 def create_an_array
-  
-end
+  sailors = ["Venus", "Mercury", "Mars", "Jupiter"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  numbers = [1,2,3,4]
+  numbers.push("arrays!")
+  end
 
 def add_element_to_start_of_array(array, element)
-  
+  numbers = [1,2,3,4,5]
+  numbers.unshift("wow")
 end
+  
 
 def remove_element_from_end_of_array(array)
-  
+  count = [1,2,3,4,5, "arrays!"]
+  count_trash = count.pop
 end
+  
 
 def remove_element_from_start_of_array(array)
-  
-end
+  numbers_one = ["wow", 1,2,3,4,5]
+  numbers_trash = numbers_one.shift
+  end
 
 def retrieve_element_from_index(array, index_number)
-  
+  series = ["Well", "I", "am", "happy"]
+  series[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
-end
+  boy_says = ["wow", "thats", "crazy"]
+  boy_says.first
+  end
 
 def retrieve_last_element_from_array(array)
-  
-end
+  girl_says = ["i", "love", "arrays!"]
+  girl_says.last
+  end
